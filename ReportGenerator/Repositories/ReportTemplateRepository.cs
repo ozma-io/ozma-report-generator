b2efbd3a41da89df67e8a2f4d12143ef91471838
Nikolay Amiantov <ab@fmap.me>
2021-11-16 19:25:24 +0000

    Perhaps fix delete op

--- ReportGenerator/Repositories/ReportTemplateRepository.cs
+++ ReportGenerator/Repositories/ReportTemplateRepository.cs
@@ -42,7 +42,7 @@ namespace ReportGenerator.Repositories
 
         public async Task DeleteTemplate(int id)
         {
-            var item = dbContext.ReportTemplates.FirstOrDefault(p =>
+            var item = dbContext.VReportTemplates.FirstOrDefault(p =>
                 (p.Schema.InstanceId == instance.Id) && (p.Id == id));
             dbContext.Remove(item);
             await dbContext.SaveChangesAsync();
